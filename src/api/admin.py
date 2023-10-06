943ffd3079fccdc5eba677a79ecc9fa6fc5b320b
smbock42 <smbock42@gmail.com>
2023-10-06 01:13:37 -0700

    fixed bug

--- src/api/admin.py
+++ src/api/admin.py
@@ -18,7 +18,7 @@ def reset():
     """
     #TODO Reset carts
 
-    reset_cash_ledger_sql = f"INSERT INTO cash_ledger(type,description,amount,balance) VALUES ('reset','Reset Store - gold set to $100 and potions/barrels set to 0',100,100"
+    reset_cash_ledger_sql = f"INSERT INTO cash_ledger(type,description,amount,balance) VALUES ('reset','Reset Store - gold set to $100 and potions/barrels set to 0',100,100)"
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(reset_cash_ledger_sql))
     
@@ -41,7 +41,7 @@ def reset():
     sql = "DELETE FROM cart_items"
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(sql))
-        
+
     sql = "DELETE FROM cart_table"
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(sql))
