961992669a2d6c5b886601cad3d3a43ca08d890c
smbock42 <smbock42@gmail.com>
2023-10-25 13:59:56 -0700

    fixed catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,9 +14,8 @@ def get_catalog():
     sql = "SELECT bottle_table.name, bottle_table.sell_in_catalog, bottle_table.sku, bottle_table.price, bottle_table.r, bottle_table.g, bottle_table.b, bottle_table.d, bottle_table.make_more, SUM(bottle_ledger.amount) AS quantity FROM bottle_table INNER JOIN bottle_ledger ON bottle_table.sku = bottle_ledger.sku GROUP BY bottle_table.name, bottle_table.sku, bottle_table.price, bottle_table.sell_in_catalog, bottle_table.r, bottle_table.g, bottle_table.b, bottle_table.d, bottle_table.make_more ORDER BY quantity ASC;"
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(sql))
-
-    available_potions = []
-    potions = result.all()
+        available_potions = []
+        potions = result.all()
     for potion in potions:
         if potion.quantity > 0 and potion.sell_in_catalog == True:
             potion_info = {
